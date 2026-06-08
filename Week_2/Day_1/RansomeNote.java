29ee3dbba4240fe1a2242e107c5fecffa912d29a
Manvee1608 <manveec1608@gmail.com>
2026-06-09 00:07:44 +0530

    Update RansomeNote.java

--- Week_2/Day_1/RansomeNote.java
+++ Week_2/Day_1/RansomeNote.java
@@ -1,1 +1,27 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
 
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int n = ransomNote.length();
+
+        for (char c : ransomNote.toCharArray()) {
+            if (map.containsKey(c) && map.get(c) > 0) {
+                map.put(c, map.get(c) - 1);
+                n--;
+            } else {
+                return false;
+            }
+        }
+
+        return n == 0;
+    }
+}
