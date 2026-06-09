a16000126b26eda354f0532a42c6c6755b244434
Manvee1608 <manveec1608@gmail.com>
2026-06-09 23:26:08 +0530

    Update LonSubstring.java

--- Week_2/Day_2/LonSubstring.java
+++ Week_2/Day_2/LonSubstring.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+     int res=0; int left=0;
+     Map<Character, Integer> map= new HashMap();
+     char[] arr = s.toCharArray();
+     int right =0;
+     for(;right<arr.length;right++){
+        int id = map.getOrDefault(arr[right],-1);
+        if(id != -1 && id>=left){
+            res=Math.max(res, right-1-left+1);
+            left= id+1;
+        }
+        map.put(arr[right], right);
+     }   
+     return Math.max(res,right-1-left+1);
+    }
+}
