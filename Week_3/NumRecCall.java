1df709016874c37078a5ef85706b873ef7a4f7bc
Manvee1608 <manveec1608@gmail.com>
2026-06-19 23:30:39 +0530

    Create NumRecCall.java

---
+++ Week_3/NumRecCall.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class RecentCounter {
+
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        q.offer(t);
+        int low = t - 3000;
+
+        while (!q.isEmpty() && q.peek() < low) {
+            q.poll();
+        }
+
+        return q.size();
+    }
+}
