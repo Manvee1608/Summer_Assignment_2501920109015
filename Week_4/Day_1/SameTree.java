71f3c35892d4c10a6e32485ad83d36c8f15ef9a6
Manvee1608 <manveec1608@gmail.com>
2026-06-23 00:00:46 +0530

    Create SameTree.java

---
+++ Week_4/Day_1/SameTree.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean sameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return sameTree(p.left, q.left) && sameTree(p.right, q.right);
+    }
+}
